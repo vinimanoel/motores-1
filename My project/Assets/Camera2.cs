4f5538086ea635b75805066b8332db48b4a890e1
Vinimano <162722259+vinimanoel@users.noreply.github.com>
2025-03-07 10:26:10 -0300

    jogo tomando forma
    da dando certo

--- My project/Assets/Camera2.cs
+++ My project/Assets/Camera2.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera2 : MonoBehaviour
+public class Camera : MonoBehaviour
 {
     public Transform _alvo1;
     public Transform _alvo2;
